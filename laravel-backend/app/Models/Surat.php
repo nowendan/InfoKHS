3ae45ac19b5c585a2d5a001069d62ef833bf9dea
Marcell145 <Marcellleeviroy9@gmail.com>
2025-11-18 10:45:00 +0700

    Update Surat.php

--- laravel-backend/app/Models/Surat.php
+++ laravel-backend/app/Models/Surat.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Surat extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'surat';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'nomor_surat',
+        'nama_pemohon',
+        'jenis_surat',
+        'keterangan',
+        'status',
+        'tanggal_pengajuan',
+    ];
+
+    protected $casts = [
+        'tanggal_pengajuan' => 'datetime',
+    ];
 }
