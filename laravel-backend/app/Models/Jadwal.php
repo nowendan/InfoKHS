c73e7c456dbc0a94d1b7967c7a82562fe191728e
Grazethzune <113648882+Grazethzune@users.noreply.github.com>
2025-11-18 10:19:39 +0700

    Update Jadwal.php

--- laravel-backend/app/Models/Jadwal.php
+++ laravel-backend/app/Models/Jadwal.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Jadwal extends Model
 {
-    //
+    use HasFactory;
+    /**
+    * fillable
+    *
+    * @var array
+    */
+    protected $fillable = [
+    'NIM', //Primary key
+    'id_jadwal', //Foreign key dari tabel KRS
+    'nama_dosen',
+    'ruang',
+    'jam',
+    ];
+
 }
